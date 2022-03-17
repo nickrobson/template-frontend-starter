b601ee57f785be63fa5f9219f56f9bfaeac7b620
Nick Robson <robson.nicholas1@gmail.com>
2022-03-18 00:19:01 +1100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { styled } from '@compiled/react';
+
+const StyledH1 = styled.h1`
+  color: #333;
+`;
+
+export const App = () => {
+  return (
+    <>
+      <header>
+        <StyledH1>template-frontend-starter</StyledH1>
+      </header>
+      <main>
+        <p>
+          Hello there! This template is intended to make bootstrapping a modern
+          frontend project easy.
+        </p>
+      </main>
+    </>
+  );
+};
