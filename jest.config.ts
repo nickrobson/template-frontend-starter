b0709af6034ac79e43c99e0b89dd63edf1953bac
Nick Robson <robson.nicholas1@gmail.com>
2022-04-02 16:50:37 +1100

    clean up after each test

--- jest.config.ts
+++ jest.config.ts
@@ -1,26 +1,14 @@
 export default {
-  clearMocks: true,
   errorOnDeprecated: true,
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
   notify: true,
   notifyMode: 'failure-change',
-  resetMocks: true,
   resetModules: false,
-  restoreMocks: true,
   roots: ['./'],
   slowTestThreshold: 5,
   testLocationInResults: true,
   timers: 'fake',
   watchman: true,
-
-  collectCoverage: true,
-  collectCoverageFrom: [
-    '<rootDir>/{entry,packages}/**/*.{js,jsx,ts,tsx}',
-    '!**/node_modules/**',
-  ],
-  coverageDirectory: 'coverage',
-  coveragePathIgnorePatterns: ['/node_modules/'],
-  coverageProvider: 'v8',
 
   projects: [
     {
@@ -32,9 +20,22 @@ export default {
         '<rootDir>/{entry,packages}/**/?(*.)+(spec|test).{js,jsx,ts,tsx}',
       ],
 
+      clearMocks: true,
+      resetMocks: true,
+      restoreMocks: true,
+
       transform: {
         '\\.(ts|tsx)$': '<rootDir>/jest-transformer.js',
       },
+
+      collectCoverage: true,
+      collectCoverageFrom: [
+        '<rootDir>/{entry,packages}/**/*.{js,jsx,ts,tsx}',
+        '!**/node_modules/**',
+      ],
+      coverageDirectory: 'coverage',
+      coveragePathIgnorePatterns: ['/node_modules/'],
+      coverageProvider: 'v8',
     },
     {
       displayName: 'lint',
