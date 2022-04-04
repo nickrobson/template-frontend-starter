ea3b616b88ff50ba418ca372cfb2596665cdc4ec
Nick Robson <robson.nicholas1@gmail.com>
2022-04-04 23:58:47 +1000

    upgrade all the things

--- entry/src/index.tsx
+++ entry/src/index.tsx
@@ -1,4 +1,12 @@
-import ReactDOM from 'react-dom';
+// Temporarily include React 18 types, while @types/react does not yet support React 18 out-of-the-box
+/// <reference types="react/next" />
+import ReactDOM from 'react-dom/client';
 import { App } from './App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const container = document.getElementById('app');
+if (!container) {
+  throw new Error('Container with ID #app not found');
+}
+
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
