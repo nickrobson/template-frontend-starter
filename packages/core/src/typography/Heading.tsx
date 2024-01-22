1761217aa77dc88311877578a9f50cf6f3e02b62
Nick Robson <robson.nicholas1@gmail.com>
2024-01-22 11:42:45 +1100

    Upgrade Compiled, use css prop

--- packages/core/src/typography/Heading.tsx
+++ packages/core/src/typography/Heading.tsx
@@ -1,15 +1,29 @@
+/** @jsxImportSource @compiled/react */
 import * as React from 'react';
-import { styled } from '@compiled/react';
-
-const StyledHeading = styled.h1({
-  color: '#3300cc',
-});
+import { css } from '@compiled/react';
 
 type HeadingProps = {
   size: 1 | 2 | 3 | 4 | 5 | 6;
   children: React.ReactNode;
 };
 
-export const Heading: React.FC<HeadingProps> = ({ size, children }) => (
-  <StyledHeading as={`h${size}`}>{children}</StyledHeading>
-);
+export const Heading: React.FC<HeadingProps> = ({ size, children }) => {
+  switch (size) {
+    case 1:
+      return <h1 css={headingStyles}>{children}</h1>;
+    case 2:
+      return <h2 css={headingStyles}>{children}</h2>;
+    case 3:
+      return <h3 css={headingStyles}>{children}</h3>;
+    case 4:
+      return <h4 css={headingStyles}>{children}</h4>;
+    case 5:
+      return <h5 css={headingStyles}>{children}</h5>;
+    case 6:
+      return <h6 css={headingStyles}>{children}</h6>;
+  }
+};
+
+const headingStyles = css({
+  color: '#3300cc',
+});
